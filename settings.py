003f0e51ccdcbcf5bcded7591eeee3db4a83acca
Denis001 <Denis00-1@yandex.ru>
2022-01-22 08:47:18 +0300

    almost complete

--- settings.py
+++ settings.py
@@ -2,6 +2,12 @@ import pygame
 import sqlite3
 
 
+def insert_nick(data, id):
+    con = sqlite3.connect('settings.db')
+    con.cursor().execute(f'UPDATE nicknames SET nickname = {data} WHERE id = {id}')
+    con.commit()
+
+
 def main():
     pygame.init()
     size = width, height = 400, 600
@@ -9,23 +15,86 @@ def main():
     pygame.display.set_caption('Настройки')
     running = True
 
-    cur = sqlite3.connect('settings.db').cursor()
+    con = sqlite3.connect('settings.db')
+    cur = con.cursor()
     nick1 = cur.execute('SELECT nickname FROM nicknames WHERE id = 1').fetchone()[0]
     nick2 = cur.execute('SELECT nickname FROM nicknames WHERE id = 2').fetchone()[0]
+    sh = [cur.execute('SELECT quantity FROM ships WHERE id = 1').fetchone()[0],
+          cur.execute('SELECT quantity FROM ships WHERE id = 2').fetchone()[0],
+          cur.execute('SELECT quantity FROM ships WHERE id = 3').fetchone()[0],
+          cur.execute('SELECT quantity FROM ships WHERE id = 4').fetchone()[0],
+          cur.execute('SELECT quantity FROM ships WHERE id = 5').fetchone()[0],
+          cur.execute('SELECT quantity FROM ships WHERE id = 6').fetchone()[0],
+          cur.execute('SELECT quantity FROM ships WHERE id = 7').fetchone()[0]]
+    arrows = [pygame.Rect((160, 90), (20, 30)),
+              pygame.Rect((205, 90), (20, 30)),
+              pygame.Rect((160, 130), (20, 30)),
+              pygame.Rect((205, 130), (20, 30)),
+              pygame.Rect((160, 170), (20, 30)),
+              pygame.Rect((205, 170), (20, 30)),
+              pygame.Rect((160, 210), (20, 30)),
+              pygame.Rect((205, 210), (20, 30)),
+              pygame.Rect((160, 250), (20, 30)),
+              pygame.Rect((205, 250), (20, 30)),
+              pygame.Rect((160, 290), (20, 30)),
+              pygame.Rect((205, 290), (20, 30)),
+              pygame.Rect((290, 330), (20, 30)),
+              pygame.Rect((330, 330), (20, 30))]
+
+    back = pygame.Rect((190, 540), (200, 50))
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
-            if event.type == pygame.MOUSEMOTION:
-                pass
+                if back.collidepoint(event.pos):
+                    running = False
+                for i in range(14):
+                    if arrows[i].collidepoint(event.pos):
+                        if i % 2 == 0:
+                            zn = '-'
+                        else:
+                            zn = '+'
+                        d = i // 2
+                        now = sh[d] + 1 if zn == '+' else sh[d] - 1
+                        border = (8, 4, 3, 2, 4, 8, 8)
+                        if 0 <= now <= border[d]:
+                            cur.execute(f'UPDATE ships SET quantity = {str(now)} WHERE id = {d + 1}')
+                            con.commit()
+            sh = [cur.execute('SELECT quantity FROM ships WHERE id = 1').fetchone()[0],
+                  cur.execute('SELECT quantity FROM ships WHERE id = 2').fetchone()[0],
+                  cur.execute('SELECT quantity FROM ships WHERE id = 3').fetchone()[0],
+                  cur.execute('SELECT quantity FROM ships WHERE id = 4').fetchone()[0],
+                  cur.execute('SELECT quantity FROM ships WHERE id = 5').fetchone()[0],
+                  cur.execute('SELECT quantity FROM ships WHERE id = 6').fetchone()[0],
+                  cur.execute('SELECT quantity FROM ships WHERE id = 7').fetchone()[0]]
             screen.blit(pygame.image.load('data/background.jpg'), (0, 0))
-
+            pygame.draw.rect(screen, '#3f48cc', back, 0)
+            pygame.draw.rect(screen, 'black', back, 3)
+            for rect in arrows:
+                pygame.draw.rect(screen, '#3f48cc', rect, 0)
+                pygame.draw.rect(screen, 'black', rect, 2)
+            screen.blit(pygame.font.Font(None, 40).render('Назад', True, 'white'), (245, 552.5))
+            screen.blit(pygame.font.Font(None, 40).render(f'Игрок 1: {nick1}', True, 'white'), (10, 10))
+            screen.blit(pygame.font.Font(None, 40).render(f'Игрок 2: {nick2}', True, 'white'), (10, 50))
+            screen.blit(pygame.font.Font(None, 40).render(f'Катеры:        {sh[0]}', True, 'white'), (10, 90))
+            screen.blit(pygame.font.Font(None, 40).render(f'Эсминцы:     {sh[1]}', True, 'white'), (10, 130))
+            screen.blit(pygame.font.Font(None, 40).render(f'Крейсеры:    {sh[2]}', True, 'white'), (10, 170))
+            screen.blit(pygame.font.Font(None, 40).render(f'Линкоры:      {sh[3]}', True, 'white'), (10, 210))
+            screen.blit(pygame.font.Font(None, 40).render(f'Баржи:          {sh[4]}', True, 'white'), (10, 250))
+            screen.blit(pygame.font.Font(None, 40).render(f'Мины:            {sh[5]}', True, 'white'), (10, 290))
+            screen.blit(pygame.font.Font(None, 40).render(f'Минные тральщики:    {sh[6]}', True, 'white'), (10, 330))
         pygame.display.flip()
     pygame.quit()
 
 
-if __name__ == '__main__':
-    main()
+cur = sqlite3.connect('settings.db')
+STNGS = [cur.execute('SELECT quantity FROM ships WHERE id = 1').fetchone()[0],
+         cur.execute('SELECT quantity FROM ships WHERE id = 2').fetchone()[0],
+         cur.execute('SELECT quantity FROM ships WHERE id = 3').fetchone()[0],
+         cur.execute('SELECT quantity FROM ships WHERE id = 4').fetchone()[0],
+         cur.execute('SELECT quantity FROM ships WHERE id = 5').fetchone()[0],
+         cur.execute('SELECT quantity FROM ships WHERE id = 6').fetchone()[0],
+         cur.execute('SELECT quantity FROM ships WHERE id = 7').fetchone()[0]]
+print(STNGS)
--- training.py
+++ training.py
@@ -1,26 +1,140 @@
 import pygame
+from board import Board
+from game import MissSprite, ShotSprite
+from random import choice
 
 
-def main():
+PLACEMENTS = {
+    1: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+    2: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+    3: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+}
+
+
+def main(lvl):
     pygame.init()
     size = width, height = 500, 500
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Морской бой')
     running = True
+    clock = pygame.time.Clock()
+    curr_move = 1
+
+    sh = []
+
+    destroyed = []
+    ships = [ship for ship in sh if ship.type == 'ship']
+    mines = [ship for ship in sh if ship.type == 'mine']
+
+    mainboard = Board(10, 10)
+    mainboard.set_view(110, 50, 30)
+    mainboard.board = PLACEMENTS[lvl]
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
-            if event.type == pygame.MOUSEMOTION:
-                pass
+                for event in pygame.event.get():
+                    hit = False
+                    hitm = False
+                    hitms = False
+                    coord = ''
+                    x, y = 0, 0
+                    if event.type == pygame.QUIT:
+                        quit()
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if pygame.Rect((50, 110), (300, 300)).collidepoint(event.pos):
+                            i, j = mainboard.get_cell(event.pos)
+                            cell = mainboard.board[j][i]
+                            x, y = 50 + 30 * i, 110 + 30 * j
+                            if cell == '.':
+                                MissSprite(x, y)
+                                continue
+                            elif cell == 0:
+                                mainboard.board[j][i] = '.'
+                                MissSprite(x, y)
+                                curr_move = 2
+                            else:
+                                hit = True
+                                mainboard.board[j][i] = 'X'
+                                ShotSprite(x, y)
+                                for el in sh:
+                                    for c in el.coords:
+                                        if c == (j, i):
+                                            abc = 'ABCDEFGHIJ'
+                                            if el.type == 'mine':
+                                                hitm = True
+                                                if ships:
+                                                    coord = choice(choice(ships).coords)
+                                                    coord = f'На {abc[coord[0]] + str(coord[1] + 1)} есть корабль врага!'
+                                            elif el.type == 'minesweeper':
+                                                hitms = True
+                                                if mines:
+                                                    coord = choice(choice(mines).coords)
+                                                    coord = f'На {abc[coord[0]] + str(coord[1] + 1)} есть мина врага!'
+                                            el.coords.remove(c)
+                                            if not el.coords:
+                                                destroyed1.append(el)
+                                                for jj, ii in el.auracoords:
+                                                    mainboard.board[jj][ii] = '.'
+                                                    MissSprite(50 + 30 * ii, 110 + 30 * jj)
+                                                if set(ships) == set(destroyed):
+                                                    running = False
+                                                continue
+                                            continue
+
             screen.blit(pygame.image.load('data/background.jpg'), (0, 0))
+
+            mainboard.render(screen, 'white', 2)
+
+            screen.blit(pygame.font.Font(None, 40).render('A', True, 'white'), (50, 80))
+            screen.blit(pygame.font.Font(None, 40).render('B', True, 'white'), (80, 80))
+            screen.blit(pygame.font.Font(None, 40).render('C', True, 'white'), (110, 80))
+            screen.blit(pygame.font.Font(None, 40).render('D', True, 'white'), (140, 80))
+            screen.blit(pygame.font.Font(None, 40).render('E', True, 'white'), (170, 80))
+            screen.blit(pygame.font.Font(None, 40).render('F', True, 'white'), (200, 80))
+            screen.blit(pygame.font.Font(None, 40).render('G', True, 'white'), (230, 80))
+            screen.blit(pygame.font.Font(None, 40).render('H', True, 'white'), (260, 80))
+            screen.blit(pygame.font.Font(None, 40).render('I', True, 'white'), (290, 80))
+            screen.blit(pygame.font.Font(None, 40).render('J', True, 'white'), (320, 80))
+            screen.blit(pygame.font.Font(None, 40).render('1', True, 'white'), (20, 110))
+            screen.blit(pygame.font.Font(None, 40).render('2', True, 'white'), (20, 140))
+            screen.blit(pygame.font.Font(None, 40).render('3', True, 'white'), (20, 170))
+            screen.blit(pygame.font.Font(None, 40).render('4', True, 'white'), (20, 200))
+            screen.blit(pygame.font.Font(None, 40).render('5', True, 'white'), (20, 230))
+            screen.blit(pygame.font.Font(None, 40).render('6', True, 'white'), (20, 260))
+            screen.blit(pygame.font.Font(None, 40).render('7', True, 'white'), (20, 290))
+            screen.blit(pygame.font.Font(None, 40).render('8', True, 'white'), (20, 320))
+            screen.blit(pygame.font.Font(None, 40).render('9', True, 'white'), (20, 350))
+            screen.blit(pygame.font.Font(None, 40).render('10', True, 'white'), (20, 380))
 
         pygame.display.flip()
     pygame.quit()
-
-
-if __name__ == '__main__':
-    main()
